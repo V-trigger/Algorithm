4fc7471c398a7a6f5e52a11b2fbb2c5a710cf0a7
V-trigger <1079874948@qq.com>
2020-08-09 23:47:56 +0800

    八皇后问题 回溯

--- src/main/java/com/recursion/EightQueen2.java
+++ src/main/java/com/recursion/EightQueen2.java
@@ -27,8 +27,6 @@ public class EightQueen2 {
 
     public int count = 0;
 
-
-
     public static void main(String[] args) {
         EightQueen2 eightQueen2 = new EightQueen2();
         eightQueen2.setQueen(0);
