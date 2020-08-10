10569bc81385d6bc3faf642d7eb73fe6c1e7d51d
V-trigger <1079874948@qq.com>
2020-08-11 00:24:12 +0800

    选择排序

--- src/main/java/com/sort/BubbleSorting.java
+++ src/main/java/com/sort/BubbleSorting.java
@@ -130,7 +130,6 @@ public class BubbleSorting {
 
 
     public static void swap(int arr[], int i, int j){
-        if(i == j) return;
         arr[i] = arr[i] + arr[j];
         arr[j] = arr[i] - arr[j];
         arr[i] = arr[i] - arr[j];
