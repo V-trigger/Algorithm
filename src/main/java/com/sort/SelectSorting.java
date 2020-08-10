611e0226da1195b89913621db39b9ea6c43eb968
V-trigger <1079874948@qq.com>
2020-08-11 00:13:09 +0800

    选择排序

---
+++ src/main/java/com/sort/SelectSorting.java
@@ -0,0 +1,54 @@
+package com.sort;
+
+import java.util.Arrays;
+
+/**
+ *
+ * 选择排序
+ *    第一次从待排序的数据元素中选出最小的一个元素
+ *    存放在序列的起始位置
+ *    然后再从剩余的未排序元素中寻找到最小元素
+ *    然后放到已排序的序列的末尾
+ *
+ *    以此类推，直到全部待排序的数据元素的个数为零。选择排序是不稳定的排序方法
+ *
+ */
+public class SelectSorting {
+
+    public static void main(String[] args) {
+        int arr[] = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int) (Math.random() * 80000);
+        }
+
+        long l = System.currentTimeMillis();
+//        System.out.println(Arrays.toString(arr));
+        SelectSorting.selectSort(arr);
+//        System.out.println(Arrays.toString(arr));
+        System.out.println(System.currentTimeMillis() - l );
+
+    }
+
+
+    //选择排序
+    public static void selectSort(int arr[]){
+        int min;  //待排序数组的最小值的下标
+        for (int i = 0; i < arr.length; i++) {
+            min = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if(arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            if(min != i) swap(arr, i, min);
+        }
+    }
+
+
+    public static void swap(int arr[], int i, int j){
+        arr[i] = arr[i] + arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] = arr[i] - arr[j];
+    }
+
+}
