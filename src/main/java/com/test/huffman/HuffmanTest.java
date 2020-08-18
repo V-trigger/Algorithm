b988e69387d9f331d5320c4decd026aa56653d64
V-trigger <1079874948@qq.com>
2020-08-18 23:53:21 +0800

    赫夫曼编码 压缩测试

--- src/main/java/com/test/huffman/HuffmanTest.java
+++ src/main/java/com/test/huffman/HuffmanTest.java
@@ -4,6 +4,7 @@ import com.tree.HuffmanDecode;
 import com.tree.HuffmanEncode;
 import org.junit.Test;
 
+import java.io.*;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -28,8 +29,78 @@ public class HuffmanTest {
         Map<Character, String> huffmanCodes = huffmanEncode.getHuffmanCodes();
 
         HuffmanDecode huffmanDecode = new HuffmanDecode(huffmanCodes, code);
-        String str1 = huffmanDecode.getStr();
+        String str1 = huffmanDecode.getContent();
         System.out.println(str1);
+    }
+
+    /**
+     * 测试压缩文件
+     */
+    @Test
+    public void compressFile(){
+        InputStream fileInputStream = null;
+        OutputStream outputStream = null;
+        ObjectOutputStream objectOutputStream = null;
+        try {
+            fileInputStream = new FileInputStream("C:\\Users\\PTR\\Desktop\\compress\\test.jpg");
+            byte buff[] = new byte[fileInputStream.available()];
+            fileInputStream.read(buff);
+            fileInputStream.close();
+
+            HuffmanEncode huffmanEncode = new HuffmanEncode(new String(buff));
+            byte[] code = huffmanEncode.getCode();
+            Map<Character, String> huffmanCodes = huffmanEncode.getHuffmanCodes();
+
+            outputStream = new FileOutputStream("C:\\Users\\PTR\\Desktop\\compress\\test.jpg.zip");
+            objectOutputStream = new ObjectOutputStream(outputStream);
+            objectOutputStream.writeObject(code);
+            objectOutputStream.writeObject(huffmanCodes);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+                try {
+                    objectOutputStream.close();
+                    outputStream.close();
+                    fileInputStream.close();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+        }
+
+    }
+
+    /**
+     * 测试文件解压
+     */
+    @Test
+    public void uncompressFile(){
+        InputStream inputStream = null;
+        ObjectInputStream objectInputStream = null;
+        OutputStream outputStream = null;
+        try {
+            inputStream = new FileInputStream("C:\\Users\\PTR\\Desktop\\compress\\test.jpg.zip");
+            objectInputStream = new ObjectInputStream(inputStream);
+
+            byte[] codes = (byte[]) objectInputStream.readObject();
+            Map<Character, String> huffmanCode = (Map<Character, String>) objectInputStream.readObject();
+
+            HuffmanDecode huffmanDecode = new HuffmanDecode(huffmanCode, codes);
+            String content = huffmanDecode.getContent();
+
+            outputStream = new FileOutputStream("C:\\Users\\PTR\\Desktop\\compress\\test1.jpg");
+            outputStream.write(content.getBytes());
+        } catch (Exception e){
+
+        } finally {
+            try {
+                outputStream.close();
+                objectInputStream.close();
+                inputStream.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
     }
 
 }
